230f9b235fff968d36e11b8ab5854085a4dc9661
unknown <dastagirig1996@gmail.com>
2024-06-17 15:16:58 +0530

    customizsd docs file addedd

--- EmployeeResumeRead/EmployeeSalary/settings.py
+++ EmployeeResumeRead/EmployeeSalary/settings.py
@@ -37,14 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "salary",
-    'corsheaders'
+    "salary"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -124,11 +122,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-# CORS_ALLOWED_ORIGINS = [
-#     "*"
-# ]
 
 
 DATA_UPLOAD_MAX_NUMBER_FILES = 1000
