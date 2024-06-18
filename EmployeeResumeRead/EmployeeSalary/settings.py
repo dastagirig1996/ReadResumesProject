2fcab7f8c8e395dc2f3f15ae844a1cf25b892661
unknown <dastagirig1996@gmail.com>
2024-06-18 17:39:57 +0530

    modified

--- EmployeeResumeRead/EmployeeSalary/settings.py
+++ EmployeeResumeRead/EmployeeSalary/settings.py
@@ -103,7 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 TIME_ZONE = "UTC"
 
@@ -115,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
