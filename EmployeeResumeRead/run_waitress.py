0743699494c1d53441e05e0c389a2bdeac1102b9
dastagirig1996 <111571182+dastagirig1996@users.noreply.github.com>
2024-07-12 17:47:20 +0530

    added waitress file

---
+++ EmployeeResumeRead/run_waitress.py
@@ -0,0 +1,5 @@
+from waitress import serve
+from EmployeeSalary.wsgi import application  # Replace 'myproject' with your actual project name
+
+if __name__ == '__main__':
+    serve(application, host='0.0.0.0', port=8000, channel_timeout=3600)  # Timeout in seconds
